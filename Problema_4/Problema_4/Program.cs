01ffbba2a81f99e7113e724d3e956ee3046a25e0
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-12 12:28:56 +0200

    Finish code

---
+++ Problema_4/Problema_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_4
+{
+    class Program
+    {
+        // Detreminati daca un an y este an bisect. 
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un an ");
+            int y;
+            y = int.Parse(Console.ReadLine());
+
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+                Console.WriteLine($"Anul {y} E BISECT");
+            else
+                Console.WriteLine($"Anul {y} NU E BISECT");
+            Console.ReadKey();
+        }
+    }
+}
