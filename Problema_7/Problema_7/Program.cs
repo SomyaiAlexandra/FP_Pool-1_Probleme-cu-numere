dc89dd0dffc588cf3c3ab7bd781aa313aa51eab2
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-12 12:38:22 +0200

    Finish code.

---
+++ Problema_7/Problema_7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_7
+{
+    //Se dau doua variabile numerice a si b ale carori valori
+    //sunt date de intrare. Se cere sa se inverseze valorile lor. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti 2 numere: ");
+            int a, b;
+            Console.Write("a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inainte de inversare: ");
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+
+            int aux;
+            aux = a;
+            a = b;
+            b = aux;
+
+            Console.WriteLine("Dupa inversare: ");
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
