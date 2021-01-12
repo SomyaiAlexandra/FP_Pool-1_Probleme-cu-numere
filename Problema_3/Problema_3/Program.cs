55a2f0c2842d76800ec525cac109492a31d8ab6e
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-12 12:59:41 +0200

    Finish code.

---
+++ Problema_3/Problema_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    //Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verificare daca n il divide pe k.");
+            int n, k;
+            Console.Write("Introduceti un numar: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti un numar: ");
+            k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0)
+                Console.WriteLine($"{n} DA se divide cu {k}.");
+            else
+                Console.WriteLine($"{n} NU se divide cu {k}.");
+            Console.ReadKey();
+        }
+    }
+}
